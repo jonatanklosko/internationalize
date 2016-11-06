7d4eb8a7d0df5b7086f355a72a799c74a7c7839c
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-11-06 17:56:31 +0100

    Activate the right tab on the translation page.

--- client/app/views/translations/translations.routes.js
+++ client/app/views/translations/translations.routes.js
@@ -35,13 +35,29 @@ export default ($stateProvider) => {
       abstract: true,
       template: `
         <div>
-          <md-nav-bar nav-bar-aria-label="Translation menu">
-            <md-nav-item name="translate" md-nav-sref="translations.show.translate">Translate</md-nav-item>
-            <md-nav-item name="browse" md-nav-sref="translations.show.browse">Browse</md-nav-item>
+          <md-nav-bar md-selected-nav-item="vm.currentState" nav-bar-aria-label="Translation menu">
+            <md-nav-item ng-repeat="item in vm.navItems" name="{{ item.state}} " md-nav-sref="{{ item.state }}">
+              {{ item.text }}
+            </md-nav-item>
           </md-nav-bar>
           <ui-view></ui-view>
         </div>
-      `
+      `,
+      controller: class TranslationsShowController {
+        constructor($state) {
+          'ngInject';
+
+          this.currentState = $state.current.name;
+          this.navItems = [{
+            state: 'translations.show.translate',
+            text: 'Translate'
+          }, {
+            state: 'translations.show.browse',
+            text: 'Browse'
+          }];
+        }
+      },
+      controllerAs: 'vm'
     })
     .state('translations.show.translate', {
       url: '/translate',
