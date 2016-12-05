2e9a07f557a89c240b74bd003d859bffc3bde77b
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-12-05 16:46:59 +0100

    Add hints to the translation form.

--- client/app/directives/mongoose-validations.directive.js
+++ client/app/directives/mongoose-validations.directive.js
@@ -12,7 +12,7 @@ export default ($compile) => {
         let name = input.attr('name');
 
         let messageTemplate = `<div class="error-message" md-colors="{color: 'warn-A700'}" ng-show="errors.${name}">
-                                 <small>{{ errors.${name}.message }}</small>
+                                 {{ errors.${name}.message }}
                                </div>`;
         let message = $compile($(messageTemplate))($scope);
         input.after(message);
